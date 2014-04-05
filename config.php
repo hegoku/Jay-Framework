cc4accde0d17ed39a26e5f1391147879654e795f
hegoku <hegokujack@gmail.com>
2014-04-05 11:06:21 +0800

    添加basePath和baseUrl属性
    这2个属性在config.php里设置
    
    basePath是应用在文件系统上的绝对路径
    baseUrl是应用相对于服务器的绝对路径

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 return array(
+	'basePath'=>dirname(__FILE__),
+	'baseUrl'=>dirname($_SERVER['PHP_SELF']),
 	'db'=>new Sqlite("xx.db"),
 	//'db'=>new Mysql($host,$user,$pwd,$db,$charset),
 	'ControllerDir'=>dirname(__FILE__)."/controllers",
